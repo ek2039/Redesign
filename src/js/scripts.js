a59fb2f9b525811b971329109a243a7658f170c2
Elana Korngold <elana.korngold@gmail.com>
2021-07-01 21:47:06 -0400

    in class part two

--- src/js/scripts.js
+++ src/js/scripts.js
@@ -1,12 +1,39 @@
-
 document.addEventListener("click", clickHandlers);
 
 function clickHandlers(event) {
-  console.log(event.target);
   if (event.target.matches("#pull")) {
-    document.querySelector("body").classList.toggle("show-nav");
+    showMenu();
     event.preventDefault();
   }
+  if (event.target.matches(".content-video a")) {
+    videoSwitch(event);
+    event.preventDefault();
+  }
+  if (event.target.matches(".image-tn img")) {
+    runCarousel(event);
+    event.preventDefault();
+  }
+}
+
+function runCarousel(event) {
+  const imageHref = event.target.getAttribute("src");
+  const titleText = event.target.title;
+  // document.querySelector("figure img").setAttribute("src", imageHref);
+  document.querySelector("figure img").src = imageHref;
+  document.querySelector("figcaption").innerText = titleText;
+}
+
+function showMenu() {
+  document.querySelector("body").classList.toggle("show-nav");
+}
+
+function videoSwitch(event) {
+  const iFrame = document.querySelector("iframe");
+  const videoLinks = document.querySelectorAll(".content-video a");
+  videoLinks.forEach((videoLink) => videoLink.classList.remove("active"));
+  event.target.classList.add("active");
+  const videoToPlay = event.target.getAttribute("href");
+  iFrame.setAttribute("src", videoToPlay);
 }
 
 // store the link plus the API key in a variable
@@ -39,19 +66,18 @@ function showData(stories) {
     .join("");
 
   document.querySelector(".stories").innerHTML = looped;
-  sessionStorage.setItem(sotragePrefix,looped)
+  sessionStorage.setItem(sotragePrefix, looped);
 }
 
 if (document.querySelector(".home")) {
-  let saved = sessionStorage.getItem(storagePrefix)
+  let saved = sessionStorage.getItem(storagePrefix);
   if (saved) {
     console.log("Loading from session storage");
-    document.querySelector(".stories").innerHTML=saved;
+    document.querySelector(".stories").innerHTML = saved;
   } else {
     console.log.apply("fetching from nytimes");
-    getStories()
+    getStories();
   }
-
-  }
+}
 //   getStories();
 // }
